ca0546cbc8df876da4c03869b1353f5bfc58a2d8
hirogwa <hirogwa@gmail.com>
2015-12-16 20:36:28 +0900

    add display commands

--- airtrack.py
+++ airtrack.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import click
 import re
 import subprocess
@@ -18,10 +18,23 @@ STATEMENTS = {
     'create_index_timestamp': (' CREATE INDEX airport_checkin_timestamp' +
                                '   on airport_checkin (timestamp)'),
     'register': (' INSERT INTO airport_checkin (id, timestamp, ssid)' +
-                 ' VALUES (NULL, ?, ?)')
+                 ' VALUES (NULL, ?, ?)'),
+    'count_by_ssid': ('select count(1) from airport_checkin' +
+                      '  where timestamp between ? and ?' +
+                      '  and ssid = ?')
+}
+
+HELP = {
+    'datapoint_size': 'Weight of a data point in minutes.',
+    'ssid': ('SSID to filter the data points by.'
+             + ' Defaults to the current network if not given.')
 }
 
 DB_PATH = None
+
+TIME_FORMAT_MIN = '%Y-%m-%d %H:%M'
+time_string_full = lambda x: x.isoformat(' ')
+time_string_minute = lambda x: x.strftime(TIME_FORMAT_MIN)
 
 
 @click.group()
@@ -38,6 +51,9 @@ def cli(debug):
 
 @cli.command()
 def init_db():
+    '''
+    Initializes the database at the first use.
+    '''
     with sqlite3.connect(DB_PATH) as conn:
         cur = conn.cursor()
         cur.execute(STATEMENTS['create_table'])
@@ -46,24 +62,131 @@ def init_db():
 
 @cli.command()
 def register():
-    entry = get_current_airplay()
-    if entry:
+    '''
+    Registers the current airport connection entry.
+    '''
+    ssid = get_current_ssid()
+    if ssid:
         with sqlite3.connect(DB_PATH) as conn:
-            register_current_airport(conn, *entry)
+            register_current_airport(conn, datetime.now(), ssid)
+    else:
+        click.echo('No SSID identified')
+
+
+@cli.command()
+@click.option('-s', '--since',
+              default=datetime.min.strftime(TIME_FORMAT_MIN))
+@click.option('-t', '--till',
+              default=datetime.now().strftime(TIME_FORMAT_MIN))
+@click.option('-p', '--datapoint-size', default=1, help=HELP['datapoint_size'])
+@click.option('-i', '--ssid', help=HELP['ssid'])
+def total(since, till, datapoint_size, ssid):
+    '''
+    Shows the cumulative amount of time on the specified network
+    for the specified time range.
+    '''
+    from_time = datetime.strptime(since, TIME_FORMAT_MIN)
+    to_time = datetime.strptime(till, TIME_FORMAT_MIN)
+    return sum_up(from_time, to_time, datapoint_size, ssid)
+
+
+@cli.command()
+@click.option('-p', '--datapoint-size', default=1, help=HELP['datapoint_size'])
+@click.option('-i', '--ssid', help=HELP['ssid'])
+def today(datapoint_size, ssid):
+    '''
+    Shows the cumulative amount of time on the specified network today.
+    '''
+    to_time = datetime.now()
+    from_time = datetime(to_time.year, to_time.month, to_time.day)
+    return sum_up(from_time, to_time, datapoint_size, ssid)
+
+
+@cli.command()
+@click.option('-p', '--datapoint-size', default=1, help=HELP['datapoint_size'])
+@click.option('-i', '--ssid', help=HELP['ssid'])
+def past_day(datapoint_size, ssid):
+    '''
+    Shows the cumulative amount of time on the specified network
+    for the past day.
+    '''
+    return sum_up_to_now(timedelta(days=1), datapoint_size, ssid)
+
+
+@cli.command()
+@click.option('-p', '--datapoint-size', default=1, help=HELP['datapoint_size'])
+@click.option('-i', '--ssid', help=HELP['ssid'])
+def past_week(datapoint_size, ssid):
+    '''
+    Shows the cumulative amount of time on the specified network
+    for the past week.
+    '''
+    return sum_up_to_now(timedelta(weeks=1), datapoint_size, ssid)
+
+
+@cli.command()
+@click.option('-p', '--datapoint-size', default=1, help=HELP['datapoint_size'])
+@click.option('-i', '--ssid', help=HELP['ssid'])
+def past_month(datapoint_size, ssid):
+    '''
+    Shows the cumulative amount of time on the specified network
+    for the past month.
+    '''
+    to_time = datetime.now()
+    if to_time.month == 1:
+        from_time = datetime(to_time.year - 1, 12, to_time.day,
+                             to_time.hour, to_time.minute, to_time.second,
+                             to_time.microsecond)
+    else:
+        from_time = datetime(to_time.year, to_time.month - 1, to_time.day,
+                             to_time.hour, to_time.minute, to_time.second,
+                             to_time.microsecond)
+    return sum_up(from_time, to_time, datapoint_size, ssid)
+
+
+def sum_up_to_now(delta, datapoint_size, ssid):
+    to_time = datetime.now()
+    from_time = to_time - delta
+    return sum_up(from_time, to_time, datapoint_size, ssid)
+
+
+def sum_up(from_time, to_time, datapoint_size, ssid, output=True):
+    if not ssid:
+        click.echo('SSID not specified. Setting the current network.')
+        ssid = get_current_ssid() or ''
+
+    datapoint_count = 0
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+        rs = cur.execute(STATEMENTS['count_by_ssid'], (
+            time_string_full(from_time),
+            time_string_full(to_time),
+            ssid))
+        row = rs.fetchone()
+        datapoint_count = row[0]
+
+    h, m = divmod(datapoint_count * datapoint_size, 60)
+    if output:
+        click.echo(
+            'Cumulative time on %s from %s to %s is %02dh:%02dm'
+            % (ssid,
+               time_string_minute(from_time),
+               time_string_minute(to_time),
+               h, m))
+    return h, m
 
 
 def register_current_airport(conn, timestamp, ssid):
     cur = conn.cursor()
-    cur.execute(STATEMENTS['register'], (timestamp, ssid))
+    cur.execute(STATEMENTS['register'],
+                (time_string_full(timestamp), ssid))
 
 
-def get_current_airplay():
+def get_current_ssid():
     cmd_out = subprocess.run(
         [AIR_COMMAND, '-I'], stdout=subprocess.PIPE).stdout
     ssid_entry = re.search('\sSSID:.*\n', cmd_out.decode('utf-8'))
     if ssid_entry:
-        ssid = ssid_entry.group().split('SSID: ')[1].rstrip('\n')
-        return (datetime.now(), ssid)
+        return ssid_entry.group().split('SSID: ')[1].rstrip('\n')
     else:
-        print('No SSID identified')
         return None
